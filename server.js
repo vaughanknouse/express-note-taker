29a7b3f0661d3d5a7c2204fd16a81f39ca88f803
Vaughan Knouse <vaughanknouse@gmail.com>
2024-06-20 19:07:04 -0400

    fixed issue with css file name

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// Wildcard GET Route for homepage
+// GET Route for homepage
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
